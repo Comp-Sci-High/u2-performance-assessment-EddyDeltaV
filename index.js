ed8049e6404a74347e8d170cfbc6adc2e1c82b86
EddyDeltaV <Eduardo.santos25@compscihigh.org>
2024-11-13 15:47:13 +0000

    first function called and declared

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-
+async function fetchSpotifyPlaylists(requestURL) {
+    let request = await fetch(requestURL)
+    let data = await request.json()
+    console.log(data)
+}
+fetchSpotifyPlaylists("https://api.spotify.com/v1/users/{user_id}/playlists")
